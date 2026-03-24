a1771dbd7cdb636369410479a62a1978df05a626
kinglion <sangerenba@gmail.com>
2026-03-24 10:55:51 +0800

    Initial Commit

---
+++ GoldTracker/GoldTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoldTrackerApp.swift
+//  GoldTracker
+//
+//  Created by clintlin on 2026/3/24.
+//
+
+import SwiftUI
+
+@main
+struct GoldTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
